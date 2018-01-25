f4fa885327f422058947ace0b01ede74068018eb
thatdania@gmail.com <thatdania@gmail.com>
2018-01-25 19:02:05 +0000

    Express can now say hello
    It has two get request and the readme will be in the instructions

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req,res) {
-  res.send('Hello world')
+app.get('/', function(req,res){
+  res.send('Hello I am Browser. You are? Psst time in the query')
 })
 
+app.get('/hello/:name_person?', function(req,res) {
+  var name_person = req.params.name_person;
+  res.send('Hello ' + name_person)
+})
 
 app.listen(5000, function() {
   console.log('server running on port 5000')
